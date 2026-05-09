ae88bee5c6fb37ae77c5911f25357b5f1711e3fd
saniya <saniyagarg9@gmail.com>
2026-05-09 23:27:55 +0530

    done

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: "mysql",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false
-    }
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
-});
+);
 
 const dbConnection = async () => {
   try {
